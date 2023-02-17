63d604961e64a206dc60ebc40c9512a65022d00d
Ales Krivoruchko <jeremieua@gmail.com>
2023-02-17 12:20:56 +0300

    init commit

---
+++ task_13/Program.cs
@@ -0,0 +1,28 @@
+﻿/*13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int num;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out num);
+
+int num3(int num)
+{
+    while (num > 999)
+    {
+    num /=10;
+    }
+    return num % 10;
+}
+
+if (num < 100)
+{
+    Console.WriteLine("Nретьей цыфры нет");
+}
+else
+{
+    Console.WriteLine(num3(num));
+}
---
+++ task_15/Program.cs
@@ -0,0 +1,23 @@
+﻿/*15 Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int number;
+Console.Write($"Введите число, соответствующее дню недели: ");
+int.TryParse(Console. ReadLine()!, out number);
+
+if (number > 7 || number < 1)
+{
+Console.Write($"Вы ввели неверное число");
+}
+else if (number >= 1 && number <=5 )
+{
+Console.Write("Будный день");
+}
+else if (number >= 6 && number <= 7)
+{
+Console.Write("Выходной");
+}
